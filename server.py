f0239e02fb824b050a7a67a8824150819e5e18a7
Arterialist <qazedcwsxrfv228@gmail.com>
2019-05-21 15:39:54 +0300

    added relay mode

--- server.py
+++ server.py
@@ -18,8 +18,12 @@ class SignalingServer(WebSocket):
     def handleMessage(self):
         if is_debug:
             print("handleMessage")
-        print(f"New Message: {self.data}")
         address = (self.address[0], self.address[1])
+        print(f"New Message from {address[0]}{address[1]}: {self.data}")
+
+        if address not in peers:
+            return
+
         if is_interactive:
             response = input()
         else:
@@ -28,17 +32,21 @@ class SignalingServer(WebSocket):
         for peer_address in peers:
             if peer_address != address:
                 peers.get(peer_address)["socket"].sendMessage(response)
+                if is_relay:
+                    break
 
     def handleConnected(self):
         if is_debug:
             print("handleConnected")
-        address = (self.address[0], self.address[1])
-        peer = Client(self.address[0], self.address[1])
-        peers[address] = {
-            "peer": peer,
-            "socket": self
-        }
-        print(f"Client {self.address[0]}:{self.address[1]} connected")
+        if not is_relay:
+            if len(peers) == 2:
+                address = (self.address[0], self.address[1])
+                peer = Client(self.address[0], self.address[1])
+                peers[address] = {
+                    "peer": peer,
+                    "socket": self
+                }
+                print(f"Client {self.address[0]}:{self.address[1]} connected")
 
     def send(self, data):
         self.sendMessage(str(data))
@@ -47,11 +55,13 @@ class SignalingServer(WebSocket):
 parser = argparse.ArgumentParser(description="Server Parameters")
 parser.add_argument('--port', type=int, default=4096, help="Server listening port")
 parser.add_argument('--debug', help="Enable debug logs", action="store_true")
+parser.add_argument('--relay', help="Act as relay", action="store_true")
 parser.add_argument('--interactive', help="Enable interactive response", action="store_true")
 
 args = parser.parse_args()
 port = args.port
 is_debug = args.debug
+is_relay = args.relay
 is_interactive = args.interactive
 
 server = SimpleWebSocketServer('', port, SignalingServer)
